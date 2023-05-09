ad7911321aeb8bd568608a0b471daa76b72e575a
cyberraya7 <123887753+cyberraya7@users.noreply.github.com>
2023-05-09 15:42:42 +0900

    Update myelectionapp.py

--- code/myelectionapp.py
+++ code/myelectionapp.py
@@ -20,28 +20,34 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # --- NAVIGATION MENU ---
 selected = option_menu(
     menu_title=None,
-    options=["Parlimen", "DUN"],
-    icons=["bi-bank", "bi-house"],  # https://icons.getbootstrap.com/
+    options=["PRU-PARLIMEN", "PRN-DUN", "AHLI PARLIMEN"],
+    icons=["bi-bank", "bi-house", "bar-chart-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
 )
 
 @st.cache_data(persist=True)
 def load_data_parlimen():
-    df = pd.read_csv('./data/pru15_result_parlimen.csv')
+    df = pd.read_csv('data/pru15_result_parlimen.csv')
     df.drop(['Unnamed: 0'], axis=1, inplace=True)
     return df
 
 def load_data_dun():
-    df = pd.read_csv('./data/prn-2018.csv')
+    df = pd.read_csv('data/prn-2018.csv')
+    df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    return df
+
+def load_data_ahli_parlimen():
+    df = pd.read_csv('data/Ahli_Parlimen_15.csv')
     df.drop(['Unnamed: 0'], axis=1, inplace=True)
     return df
 
 df_parlimen = load_data_parlimen()
 df_dun = load_data_dun()
+df_ahli = load_data_ahli_parlimen()
 
 st.title(f"My Politiko")
 
-if selected == "Parlimen":
+if selected == "PRU-PARLIMEN":
     
     st.markdown('### Keputusan PRU 15')
     state = list(df_parlimen['state'].drop_duplicates())
@@ -58,6 +64,15 @@ if selected == "Parlimen":
 
     show_filtered = st.sidebar.checkbox("Show filtered data", value=True)
 
+    # Calculate total wins by state and abbreviation
+    count_wins = df_parlimen[df_parlimen['status'] == 'win'].pivot_table(index='state', columns='abbreviation', values='status', aggfunc='count', fill_value=0)
+
+    # Calculate total wins for each abbreviation across all states
+    total_wins = count_wins.sum().astype(int)
+
+    # Add a row for total wins for all states for each abbreviation
+    total_win = count_wins._append(total_wins.rename('TOTAL'))
+
     if show_filtered:
         st.dataframe(df_parlimen[
             df_parlimen['seat_name'].isin(parlimen_name_choice) &
@@ -66,6 +81,9 @@ if selected == "Parlimen":
         ].sort_values('seat_id', ascending=True).reset_index(drop=True))
     else:
         st.dataframe(df_parlimen.sort_values('seat_id', ascending=True).reset_index(drop=True))
+    
+    st.markdown('### Keputusan Setiap Negeri')
+    st.dataframe(total_win, use_container_width=True)
 
     df_parlimen_filtered = df_parlimen[
         df_parlimen['seat_name'].isin(parlimen_name_choice) &
@@ -86,7 +104,7 @@ if selected == "Parlimen":
 
 
 
-if selected == "DUN":
+if selected == "PRN-DUN":
     
     st.markdown('### Keputusan PRN 14')
     dun_state = list(df_dun['NEGERI'].drop_duplicates())
@@ -95,13 +113,13 @@ if selected == "DUN":
 
     dun_name = list(df_dun[df_dun['NEGERI'].isin(dun_state_choice)]['NAMA DUN'].drop_duplicates())
     dun_name_choice = st.sidebar.multiselect(
-        'Nama DUN:', dun_name)
+        'Nama DUN:', dun_name, default=dun_name)
 
     dun_parti_name = list(df_dun[df_dun['NAMA DUN'].isin(dun_name_choice)]['PARTI'].drop_duplicates())
     dun_parti_name_choice = st.sidebar.multiselect(
         'Nama Parti:', dun_parti_name, default=dun_parti_name)
 
-    show_filtered = st.sidebar.checkbox("Show filtered data", value=False)
+    show_filtered = st.sidebar.checkbox("Show filtered data", value=True)
 
     if show_filtered:
         st.dataframe(df_dun[
@@ -128,3 +146,60 @@ if selected == "DUN":
     ).interactive()
 
     st.altair_chart(e, use_container_width=True, theme="streamlit")
+
+if selected == "AHLI PARLIMEN":
+    
+    st.markdown('### Data Ahli Parlimen')
+    ahli_state = list(df_ahli['Negeri'].drop_duplicates())
+    ahli_state_choice = st.sidebar.multiselect(
+         "Negeri:", ahli_state, default=ahli_state)
+
+    ahli_parlimen_place = list(df_ahli[df_ahli['Negeri'].isin(ahli_state_choice)]['Kawasan Parlimen'].drop_duplicates())
+    ahli_parlimen_place_choice = st.sidebar.multiselect(
+         'Kawasan Parlimen:', ahli_parlimen_place, default=ahli_parlimen_place)
+
+    ahli_parti_name = list(df_ahli[df_ahli['Kawasan Parlimen'].isin(ahli_parlimen_place_choice)]['Parti'].drop_duplicates())
+    ahli_parti_name_choice = st.sidebar.multiselect(
+         'Nama Parti:', ahli_parti_name, default=ahli_parti_name)
+
+    show_filtered = st.sidebar.checkbox("Show filtered data", value=True)
+
+    if show_filtered:
+        st.dataframe(df_ahli[
+            df_ahli['Kawasan Parlimen'].isin(ahli_parlimen_place_choice) &
+            df_ahli['Negeri'].isin(ahli_state_choice) &
+            df_ahli['Parti'].isin(ahli_parti_name_choice)
+    ].sort_values('Negeri', ascending=True).reset_index(drop=True))
+    else:
+        st.dataframe(df_ahli.sort_values('Negeri', ascending=True).reset_index(drop=True))
+
+    df_ahli_filtered = df_ahli[
+        df_ahli['Kawasan Parlimen'].isin(ahli_parlimen_place_choice) &
+        df_ahli['Negeri'].isin(ahli_state_choice) &
+        df_ahli['Parti'].isin(ahli_parti_name_choice)
+    ]
+
+    st.markdown('### Keputusan PRN 14')
+
+    
+    # text_nama = df_ahli_filtered['Yang Berhormat'].tolist()[0]
+    # text_jawatan = df_ahli_filtered['Jawatan'].tolist()[0]
+    # text_adun = df_ahli_filtered['ADUN'].tolist()[0]
+    # st.markdown(f"<p style='font-size:24px; font-family: Arial'>{text_nama}</p>", unsafe_allow_html=True)
+    # st.markdown(f"<p style='font-size:24px; font-family: Arial'>{text_jawatan}</p>", unsafe_allow_html=True)
+    # st.markdown(f"<p style='font-size:24px; font-family: Arial'>{text_adun}</p>", unsafe_allow_html=True)
+
+    
+    # f = alt.Chart(df_ahli_filtered).mark_bar().encode(
+    #     x=alt.X('NAMA CALON', sort=alt.EncodingSortField('-BILANGAN UNDI')),
+    #     y=alt.Y('BILANGAN UNDI'),
+    #     color=alt.Color('PARTI'),
+    #     tooltip=['BILANGAN UNDI','JUMLAH UNDI','STATUS']
+    # ).interactive()
+
+    # st.altair_chart(f, use_container_width=True, theme="streamlit")
+
+
+    
+
+
